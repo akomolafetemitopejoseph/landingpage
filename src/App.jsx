bb2804f63af9caa137e8883e8137420a3c17eddf
akomolafetemitopejoseph <134941960+akomolafetemitopejoseph@users.noreply.github.com>
2025-08-10 12:17:35 -0700

    Added princing section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Navbar from "./Component/navbar";
 import HeroSection from "./Component/HeroSection";
 import FeaturesSection from "./Component/FeaturesSection";
 import WorkFlow from "./Component/WorkFlow";
+import Pricing from "./Component/Pricing";
+
 
 const App = () => {
   return (
@@ -11,6 +13,7 @@ const App = () => {
       <HeroSection />
       <FeaturesSection />
       <WorkFlow />
+      <Pricing />
     </div>
   );
 };
---
+++ src/Component/Pricing.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { BadgeCheck } from "lucide-react";
+
+const Pricing = () => {
+  const priceOptions = [
+    {
+      title: "free",
+      price: "$0",
+      features: [
+        "Private board sharing",
+        "5 Gb storage",
+        "We Analytics",
+        "Private Modes",
+      ],
+    },
+    {
+      title: "pro",
+      price: "$10",
+      features: [
+        "Private board sharing",
+        "10 Gb storage",
+        "Web Analytics (Advance)",
+        "Private Modes",
+      ],
+    },
+    {
+      title: "enterprise",
+      price: "$200",
+      features: [
+        "Private board sharing",
+        "Unlimited storage",
+        "High performance network",
+        "Private Modes",
+      ],
+    },
+  ];
+  return (
+    <div className="mt-20">
+      <h2 className="text-3xl sm:text-5xl lg:6xl text-center my-8 tracking-wider capitalize">
+        pricing
+      </h2>
+      <div className="flex m-5 flex-wrap lg:flex-nowrap justify-center gap-4">
+        {priceOptions.map((priceOptions, index) => (
+          <div key={index} className="w-full sm:w-1/2 lg:w-1/3">
+            <div className="p-10 border border-neutral-700 rounded-xl capitalize">
+              {" "}
+              <p className="text-4xl mb-8">
+                {priceOptions.title}
+                {priceOptions.title === "pro" && (
+                  <span className="bg-gradient-to-r from-orange-500 to-red-400 text-transparent bg-clip-text text-xl mb-4 ml-2">
+                    (Most popular)
+                  </span>
+                )}
+              </p>
+              <p className="mb-8">
+                <span className="text-5xl mt-6 mr-2">{priceOptions.price}</span>
+                <span className="text-neutral-400 tracking-tight">Month</span>
+                <ul>
+                  {priceOptions.features.map((priceOptions, index) => (
+                    <li key={index} className="mt-8 flex items-center">
+                      <BadgeCheck />
+                      <span className="ml-2">{priceOptions}</span>
+                    </li>
+                  ))}
+                </ul>
+                <a href="#" className="inline-flex justify-center items-center text-center w-full h-12 p-5 mt-20 tracking-tight text-xl hover:bg-orange-900 border border-orange-900 rounded-lg transparent duration-200 ">subscribe</a>
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Pricing;
